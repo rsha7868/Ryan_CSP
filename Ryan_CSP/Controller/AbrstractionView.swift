f11f36fa16637e98bef63516126425cc912ca4ac
Sharp <rsha7868@740s-pl-127.local>
2017-11-27 12:34:53 -0700

    SAving the data...

--- Ryan_CSP/Controller/AbrstractionView.swift
+++ Ryan_CSP/Controller/AbrstractionView.swift
@@ -8,13 +8,67 @@
 
 import UIKit
 
-class AbrstractionView: UIViewController {
+public class AbrstractionView: UIViewController, UIPageViewControllerDataSource
+{
+    private (set) lazy var orderedAbstractionViews : [UIViewController]
+    {
+        return [
+            self.newAbstractionView(abstractionLevel: "Block"),
+            self.newAbstractionView(abstractionLevel: "Java"),
+            self.newAbstractionView(abstractionLevel: "ByteCode"),
+            self.newAbstractionView(abstractionLevel: "Binary"),
+            self.newAbstractionView(abstractionLevel: "AndGate"),
+        ]
+    }()
+    
+    private func newAbstractionView(abstractionLevel : String) -> UIViewController
+    {
+        return UIStoryBoard(name: "Main", bundle: nil).instantiateViewController(withIentifier"\(abstractionLevel)ViewController")
+    }
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        dataSource = self
+        
+        if let firstViewController = orderAbstractionViews.first
+        {
+            setViewControllers([firstViewController],
+                               direction: .forward,
+                               animated: true,
+                               completion: nil)
+        }
 
         // Do any additional setup after loading the view.
     }
+    
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController:UIViewController) -> UIViewController?
+    {
+        guard let viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+        
+        else
+        {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0
+        else
+        {
+            return orderedAbstractionViews.last
+            
+        }
+        guard orderedAbstractionViews.count > previousIndex
+        else
+        {
+            return nil
+        }
+        return orderedAbstractionViews[previousIndex]
+    }
+    
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController: UI)
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
