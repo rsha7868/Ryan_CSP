1b14fb9c8631aa35c9df737e13808297b09d34ae
Sharp <rsha7868@740s-pl-127.local>
2017-11-27 13:23:44 -0700

    The functin area in my area can sometime fix.

--- Ryan_CSP/Controller/AbrstractionView.swift
+++ Ryan_CSP/Controller/AbrstractionView.swift
@@ -23,15 +23,15 @@ public class AbrstractionView: UIViewController, UIPageViewControllerDataSource
     
     private func newAbstractionView(abstractionLevel : String) -> UIViewController
     {
-        return UIStoryBoard(name: "Main", bundle: nil).instantiateViewController(withIentifier"\(abstractionLevel)ViewController")
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier"\(abstractionLevel)ViewController")
     }
 
-    override func viewDidLoad()
+    public override func viewDidLoad()
     {
         super.viewDidLoad()
         dataSource = self
         
-        if let firstViewController = orderAbstractionViews.first
+        if let firstViewController = orderedAbstractionViews.first
         {
             setViewControllers([firstViewController],
                                direction: .forward,
@@ -67,23 +67,37 @@ public class AbrstractionView: UIViewController, UIPageViewControllerDataSource
         return orderedAbstractionViews[previousIndex]
     }
     
-    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController: UI)
-    
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController?
+    {
+        guard let viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+        else
+        {
+            return nil
+        }
+        let nextIndex = viewControllerIndex + 1
+        
+        guard nextIndex < orderedAbstractionViews.count
+        else
+        {
+            return orderedAbstractionViews.first
+        }
+        return orderedAbstractionViews[nextIndex]
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    public func presentationCount(for pageViewController: UIPageViewController) -> Int
+    {
+        return orderedAbstractionViews.count
     }
-    */
+    
+    public func  presentationIndex(for pageViewController: UIPageViewController) -> Int
+    {
+        guard let firstViewController = ViewControllers?.first, let firstViewContollerIndex = orderedAbstractionViews.index(of: firstViewController)
+        else
+        {
+            return 0
+        }
+        return firstViewControllerIndex
+    }
 
 }
