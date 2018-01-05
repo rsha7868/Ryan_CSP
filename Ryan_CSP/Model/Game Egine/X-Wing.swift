48342863a9ece822c85df024b88d7dfd223ea2cc
Sharp <rsha7868@740s-pl-127.local>
2018-01-05 13:09:46 -0700

    Saving the wnding war.

--- Ryan_CSP/Model/Game Egine/X-Wing.swift
+++ Ryan_CSP/Model/Game Egine/X-Wing.swift
@@ -6,10 +6,11 @@
 //  Copyright © 2017 Sharp, Ryan. All rights reserved.
 //
 
-import UIKit
-public class  X-Wing : StarWars
-{
-    private var wingColor :UIColor
-    //Mark: X-Wng will have great Colors
-    public var
-}
+//import UIKit
+//public class  X-Wing : StarWars
+//{
+//    private var wingColor :UIColor
+//    //Mark: X-Wng will have great Colors
+//    public var
+//}
+
